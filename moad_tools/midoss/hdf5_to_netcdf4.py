ed36585c8fee80dff121222d25b6429f93ddc84c
Doug Latornell <dlatornell@eoas.ubc.ca>
2018-11-20 18:08:54 -0800

    Continue development of midoss.hdf5_to_netcdf4(); WIP.
    This version writes time step files for outside processing with
    nrcat and ncks.
    
    It also includes execution timing decorators.

--- moad_tools/midoss/hdf5_to_netcdf4.py
+++ moad_tools/midoss/hdf5_to_netcdf4.py
@@ -14,7 +14,9 @@
 # limitations under the License.
 """Functions to transform an MOHID HDF5 output file into an xarray dataset.
 """
+import functools
 import logging
+import time
 from types import SimpleNamespace
 
 import arrow
@@ -26,30 +28,71 @@ import xarray
 logging.getLogger(__name__)
 
 
-def hdf5_to_netcdf4(hdf5_file, nc_file):
+t0 = 0
+
+
+def main_timer(func):
+    @functools.wraps(func)
+    def wrapper_main_timer(*args, **kwargs):
+        global t0
+        t0 = time.time()
+        return_value = func(*args, **kwargs)
+        logging.info(f"total time: {time.time() - t0}")
+        return return_value
+
+    return wrapper_main_timer
+
+
+def function_timer(func):
+    @functools.wraps(func)
+    def wrapper_function_timer(*args, **kwargs):
+        t_func = time.time()
+        return_value = func(*args, **kwargs)
+        logging.info(
+            f"function time: {time.time() - t_func}; total time: {time.time() - t0}"
+        )
+        return return_value
+
+    return wrapper_function_timer
+
+
+@main_timer
+def hdf5_to_netcdf4(hdf5_file, nc_file_dir, nc_filename_root):
     """Transform selected contents of a MOHID HDF5 results file into a netCDF4 file.
 
     :param hdf5_file: File path and name of MOHID HDF5 results file to read from.
     :type hdf5_file: :py:class:`pathlib.Path` or str
 
-    :param nc_file: File path and name of netCDF4 file to write to.
-    :type nc_file: :py:class:`pathlib.Path` or str
+    :param nc_file_dir: File path and name of netCDF4 file to write to.
+    :type nc_file_dir: :py:class:`pathlib.Path` or str
+
+    :param nc_filename_root:
     """
     with tables.open_file(hdf5_file) as h5file:
         logging.info(f"reading MOHID results from: {hdf5_file}")
-        grid_indices = _init_dataset(h5file, nc_file)
-        for index in range(2, h5file.root.Time._v_nchildren + 1):
-            # for index in range(2, 11):
-            _extend_dataset(grid_indices, h5file, index, nc_file)
-        _finalize_dataset(grid_indices, h5file, nc_file)
+        timestep_files = []
+        grid_indices, timestep_file = _init_dataset(
+            h5file, nc_file_dir, nc_filename_root
+        )
+        timestep_files.append(timestep_file)
+        # for index in range(2, h5file.root.Time._v_nchildren + 1):
+        for index in range(2, 4):
+            timestep_files.append(
+                _calc_timestep_file(
+                    grid_indices, h5file, index, nc_file_dir, nc_filename_root
+                )
+            )
+        # oil_times_file = _calc_oil_times_file(grid_indices, h5file, nc_file_dir, nc_filename_root)
 
 
-def _init_dataset(h5file, nc_file):
+@function_timer
+def _init_dataset(h5file, nc_file_dir, nc_filename_root):
     time_coord = _calc_time_coord(h5file, 1)
     logging.info(f"initializing dataset with fields at: {time_coord.values[0]}")
     z_index, y_index, x_index = _calc_zyx_indices(h5file)
     logging.info(
-        f"initializing dataset with (z, y, x) grid indices: ({z_index.size}, {y_index.size}, {x_index.size})"
+        f"initializing dataset with (z, y, x) grid indices: "
+        f"({z_index.size}, {y_index.size}, {x_index.size})"
     )
     data_vars = {}
     for group in h5file.root.Results.OilSpill.Data_2D:
@@ -75,14 +118,19 @@ def _init_dataset(h5file, nc_file):
             x_index.name: x_index,
         },
     )
+    timestamp = (
+        time_coord.values[0].astype("datetime64[m]").astype(str).replace(":", "")
+    )
+    nc_file = nc_file_dir / f"{nc_filename_root}_{timestamp}.nc"
     _write_netcdf(ds, nc_file)
-    logging.info(f"wrote initialized dataset to: {nc_file}")
-    return SimpleNamespace(z_index=z_index, y_index=y_index, x_index=x_index)
+    logging.info(f"wrote initial time step to: {nc_file}")
+    return SimpleNamespace(z_index=z_index, y_index=y_index, x_index=x_index), nc_file
 
 
-def _extend_dataset(grid_indices, h5file, index, nc_file):
+@function_timer
+def _calc_timestep_file(grid_indices, h5file, index, nc_file_dir, nc_filename_root):
     time_coord = _calc_time_coord(h5file, index)
-    logging.info(f"extending dataset with fields at: {time_coord.values[0]}")
+    logging.info(f"processing fields at: {time_coord.values[0]}")
     data_vars = {}
     for group in h5file.root.Results.OilSpill.Data_2D:
         if group._v_name in ("Beaching Time", "Oil Arrival Time"):
@@ -111,7 +159,7 @@ def _extend_dataset(grid_indices, h5file, index, nc_file):
         logging.debug(
             f"added (t, z, y, x) field: {group._v_name} at {time_coord.values[0]}"
         )
-    grid_indices = xarray.Dataset(
+    ds = xarray.Dataset(
         data_vars=data_vars,
         coords={
             time_coord.name: time_coord,
@@ -120,13 +168,16 @@ def _extend_dataset(grid_indices, h5file, index, nc_file):
             grid_indices.x_index.name: grid_indices.x_index,
         },
     )
-    with xarray.open_dataset(nc_file, engine="h5netcdf") as stored_ds:
-        extended_ds = xarray.concat((stored_ds, grid_indices), dim="time")
-    _write_netcdf(extended_ds, nc_file)
-    logging.debug(f"wrote extended dataset to: {nc_file}")
+    hr = time_coord.values[0].astype("datetime64[m]").astype(str)
+    nc_file = nc_file_dir / f"{nc_filename_root}_{hr}.nc"
+    _write_netcdf(ds, nc_file)
+    logging.debug(f"wrote time step to: {nc_file}")
+    return nc_file
 
 
-def _finalize_dataset(grid_indices, h5file, nc_file):
+@function_timer
+def _calc_oil_times_file(grid_indices, h5file, nc_file_dir, nc_filename_root):
+    logging.info(f"processing oil beaching and arrival times")
     data_vars = {}
     for group in h5file.root.Results.OilSpill.Data_2D:
         if group._v_name not in ("Beaching Time", "Oil Arrival Time"):
@@ -139,12 +190,17 @@ def _finalize_dataset(grid_indices, h5file, nc_file):
         logging.info(
             f"added (y, x) field: {group._v_name} at time step {group._v_nchildren}"
         )
-    with xarray.open_dataset(nc_file, engine="h5netcdf") as stored_ds:
-        stored_ds.load()
-        for name, array in data_vars.items():
-            stored_ds[name] = array
-    _write_netcdf(stored_ds, nc_file)
-    logging.info(f"wrote finalized dataset to: {nc_file}")
+    ds = xarray.Dataset(
+        data_vars=data_vars,
+        coords={
+            grid_indices.y_index.name: grid_indices.y_index,
+            grid_indices.x_index.name: grid_indices.x_index,
+        },
+    )
+    nc_file = nc_file_dir / f"{nc_filename_root}_oil_times.nc"
+    _write_netcdf(ds, nc_file, time_coord=False, scaled_vars=False)
+    logging.debug(f"wrote oil beaching and arrival times to: {nc_file}")
+    return nc_file
 
 
 def _calc_time_coord(h5file, index):
@@ -217,19 +273,26 @@ def _calc_data_var(group, index, coords):
     }
 
 
-def _write_netcdf(ds, nc_file):
-    encoding = {
-        "time": {"dtype": numpy.float64, "units": "seconds since 1970-01-01T00:00:00Z"}
-    }
-    for var in ds.data_vars:
+def _write_netcdf(ds, nc_file, time_coord=True, scaled_vars=True):
+    encoding = {}
+    if time_coord:
         encoding.update(
-            {var: {"dtype": numpy.int32, "scale_factor": 1e-4, "_FillValue": -9999}}
+            {
+                "time": {
+                    "dtype": numpy.float64,
+                    "units": "seconds since 1970-01-01T00:00:00Z",
+                }
+            }
         )
+    if scaled_vars:
+        for var in ds.data_vars:
+            encoding.update(
+                {var: {"dtype": numpy.int32, "scale_factor": 1e-4, "_FillValue": -9999}}
+            )
     ds.to_netcdf(
         nc_file,
         mode="w",
         format="NETCDF4",
-        engine="h5netcdf",
         encoding=encoding,
         unlimited_dims=("time",),
         compute=True,
