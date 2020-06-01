0314a40778658143f0980ee6a616aeb9950ff184
Doug Latornell <dlatornell@eoas.ubc.ca>
2020-06-01 10:35:26 -0700

    Pull in get_date() function
    From https://github.com/MIDOSS/analysis-rachael/blob/master/notebooks/monte_carlo/get_date.py

--- moad_tools/midoss/random_oil_spills.py
+++ moad_tools/midoss/random_oil_spills.py
@@ -16,10 +16,16 @@
 of random oil spills to drive Monte Carlo runs of MOHID.
 """
 import logging
+import random
 import sys
+from array import array
+from datetime import timedelta
 from pathlib import Path
 
+import arrow
 import click
+import numpy
+import rasterio
 import yaml
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
@@ -39,6 +45,55 @@ def random_oil_spills(n_spills, config_file):
     with Path(config_file).open("rt") as f:
         config = yaml.safe_load(f)
         logging.info(f"read config dict from {config_file}")
+    start_date = arrow.get(config["start date"]).datetime
+    end_date = arrow.get(config["end date"]).datetime
+    geotiffs_dir = config["geotiffs dir"]
+    spill_date_hour = get_date(start_date, end_date, geotiffs_dir)
+
+
+def get_date(start_date, end_date, geotiff_directory):
+    # Load GeoTIFF files for each month and add up VTE
+    months = array("i", range(1, 13))
+    total_vte_by_month = []
+
+    for month in months:
+        # The filenames are formated as "all_2018_MM.tif"
+        f_name = f"{geotiff_directory}all_2018_{month:02.0f}.tif"
+
+        # open GeoTIFF file for reading
+        traffic_reader = rasterio.open(f_name)
+
+        # load data in a way that automatically closes file when finished
+        with traffic_reader as dataset:
+            # resample data to target shape
+            data = dataset.read(1)
+
+            # remove no-data values and singular dimension
+            data = numpy.squeeze(data)
+            data[data < 0] = 0
+
+            total_vte_by_month.append(data.sum())
+
+    # calculate VTE probability by month based on total traffic for each month
+    vte_probability = total_vte_by_month / numpy.sum(total_vte_by_month)
+
+    # Randomly select month based on weighting by vessel traffic
+    month_random = numpy.random.choice(months, p=vte_probability)
+
+    # Now that month is selected, we need to choose day, year, and time.  We weight these all the same
+    time_period = end_date - start_date
+    time_period_inhours = numpy.int(time_period.total_seconds() / 3600)
+    date_arr = [
+        start_date + timedelta(hours=i) for i in range(0, time_period_inhours + 1)
+    ]
+
+    # Extract dates in time period for only the month selected as month_random
+    date_arr_select = []
+    for days in date_arr:
+        if days.month == month_random:
+            date_arr_select.append(days)
+
+    return random.choice(date_arr_select)
 
 
 def write_csv_file(df, csv_file):
@@ -115,3 +170,9 @@ def cli(n_spills, config_file, csv_file, verbosity):
     )
     df = random_oil_spills(n_spills, config_file)
     write_csv_file(df, csv_file)
+
+
+# This stanza facilitates running the script in a Python debugger
+if __name__ == "__main__":
+    n_spills, config_file = sys.argv[1:]
+    random_oil_spills(int(n_spills), config_file)
