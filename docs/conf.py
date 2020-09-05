b7ace36d3ef6c9237a0ddeaf90edf83741f24dc1
Doug Latornell <dlatornell@eoas.ubc.ca>
2020-09-05 10:30:34 -0700

    Correct inter-sphinx mapping URL for shapely docs

--- docs/conf.py
+++ docs/conf.py
@@ -71,7 +71,7 @@ intersphinx_mapping = {
     "pandas": ("https://pandas.pydata.org/docs/", None),
     "python": ("https://docs.python.org/3/", None),
     "rasterio": ("https://rasterio.readthedocs.io/en/latest/", None),
-    "shapely": ("https://shapely.readthedocs.io/", None),
+    "shapely": ("https://shapely.readthedocs.io/en/latest/", None),
     "xarray": ("https://xarray.pydata.org/en/stable", None),
 }
 
