be9d3ca88a5cd1058918d89033f943d9b62c75f8
Doug Latornell <dlatornell@eoas.ubc.ca>
2024-12-12 13:44:02 -0800

    Add handling for PyTables pkg not in environment
    Handle missing PyTables package in hdf5_to_netcdf4.py module by providing a
    clear error message and exit code, ensuring graceful failure. Added unit tests
    to verify module import and CLI behaviour when PyTables is absent. This improves
    robustness and user feedback in incompatible environments.

--- tests/midoss/test_hdf5_to_netcdf4.py
+++ tests/midoss/test_hdf5_to_netcdf4.py
@@ -18,3 +18,52 @@
 
 """Unit tests for hdf5_to_netcdf4 module.
 """
+import shlex
+import sys
+
+import pytest
+
+try:
+    import tables
+
+    tables_imported = True
+except ImportError:
+    tables_imported = False
+
+
+@pytest.mark.skipif(
+    tables_imported, reason="PyTables package is in the conda environment"
+)
+class TestNoTablesPackage:
+    """Unit tests for handling of PyTables package (called `tables` in imports) not in the conda
+    environment.
+    """
+
+    def test_module_import_msg(self, monkeypatch):
+        # Monkeypatch sys.argv to an empty so that we get an import despite the test being run
+        # via pytest from the command line
+        monkeypatch.setattr(sys, "argv", [])
+
+        msg = (
+            "Please create an environment with `conda env create -f envs/environment-midoss.yaml` "
+            "to use the hdf5_to_netcdf4 module or its command-line tool"
+        )
+        with pytest.raises(ModuleNotFoundError, match=msg):
+            # noinspection PyPackageRequirements
+            pass
+
+    def test_cli_script_msg(self, monkeypatch, capsys):
+        # Monkeypatch sys.argv to simulate CLI invocation despite the test being run
+        # via pytest from the command line
+        monkeypatch.setattr(sys, "argv", shlex.split("hdf5-to-netcdd4 foo.hdf5 foo.nc"))
+
+        msg = (
+            "Please create an environment with `conda env create -f envs/environment-midoss.yaml` "
+            "to use the hdf5_to_netcdf4 module or its command-line tool\n"
+        )
+        with pytest.raises(SystemExit) as exc:
+            # noinspection PyPackageRequirements
+            pass
+
+        assert capsys.readouterr().err == msg
+        assert exc.value.code == 2
